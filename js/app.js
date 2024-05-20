4f991df9ebf85d68d0d1596bbdea484a5e13633a
danjpal16 <144719377+danjpal16@users.noreply.github.com>
2024-05-19 22:00:26 -0400

    classes names added

--- js/app.js
+++ js/app.js
@@ -5,3 +5,8 @@ const clear = document.querySelector(".clear");
 const dateElement = document.getElementById("date");
 const list = document.getElementById("list");
 const input = document.getElementById("input");
+
+// Classes names
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle-thin";
+const LINE_THROUGH = "lineThrough";
