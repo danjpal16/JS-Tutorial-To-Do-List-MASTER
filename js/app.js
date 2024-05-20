741d4b6b5c11396773cd6e7ed0a013f0134a3fc9
danjpal16 <144719377+danjpal16@users.noreply.github.com>
2024-05-19 22:47:05 -0400

    added to do function

--- js/app.js
+++ js/app.js
@@ -31,3 +31,21 @@ const options = { weekday: "long", month: "short", day: "numeric" };
 const today = new Date();
 
 dateElement.innerHTML = today.toLocaleDateString("en-US", options);
+
+// Add to do function
+function addToDo(toDo, id, done, trash) {
+    if(trash){return;}
+
+    const DONE = done ? CHECK : UNCHECK;
+    const LINE = done ? LINE_THROUGH : "";
+
+  const item = `
+    <li class="item">
+      <i class="fa ${DONE} co" job="complete" id="${id}"></i>
+      <p class="text ${LINE}">${toDo}</p>
+      <i class="fa fa-trash-o de" job="delete" id="${id}"></i>
+    </li>`;
+  const position = "beforeend";
+
+  list.insertAdjacentHTML(position, item);
+}
